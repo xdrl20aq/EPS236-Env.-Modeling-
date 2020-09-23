03eb3b84ee91b9fc4409f5ef924bb0421d537fe7
xdrl20aq <43107497+xdrl20aq@users.noreply.github.com>
2020-09-23 15:35:44 +0800

    two plots

--- Project1-1 Siyi.R
+++ Project1-1 Siyi.R
@@ -28,13 +28,24 @@ string.of.boxes = function(nbox=5, k.h=0.01, va=0.014, ini.c=1, ini.box=1,ntimes
   return(ct)
 }
 
-####matplot
-ct = string.of.boxes(ntimesteps=200)
-plot(1:(ntimesteps+1), ct[1,],col="1",ylim=0:1)
-for (i in 2:5){
-  points(1:(ntimesteps+1), ct[i,],col = i)
+####plot:concentration vs time
+nbox0 =15; ntimesteps0=500; ini.c0=5.0; ini.box0=7
+ct = string.of.boxes(ntimesteps=ntimesteps0, nbox=nbox0,ini.c=ini.c0,ini.box=ini.box0)
+# plot(1:(ntimesteps+1), ct[1,],col="1",ylim=0:1)
+# for (i in 2:nbox){
+#     points(1:(ntimesteps+1), ct[i,],col = i)
+# }
+
+####plot: concentration vs box
+
+folder="C:/Users/yueyi/Documents/project1-1"
+for(t in 1:ntimesteps0){
+  name=as.character(t)
+  png(paste(folder,"/",name ,".png",sep=""))
+  for (n in 1:nbox0){
+    plot(1:nbox0,ct[,t],ylim=c(0,ini.c0), ylab="concentration", xlab="box")
+  }
+  dev.off() 
 }
-
-
 
 
