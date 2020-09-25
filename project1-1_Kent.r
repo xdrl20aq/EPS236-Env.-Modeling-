aaa061985375863e76f01dc305117e8f0351b3c9
ktoshima <24969845+ktoshima@users.noreply.github.com>
2020-09-25 20:39:47 +0900

    remove calculating inverse and sequencing animation

--- project1-1_Kent.r
+++ project1-1_Kent.r
@@ -64,25 +64,27 @@ K = add_diffusion_term(K, k.h.dt, n_box)
 eigens = eigen(K)
 e_vals = eigens$values
 Xe = eigens$vectors
-Xe.inv = solve(Xe)
 
 # initial condition
 C_0 = matrix(0, n_box, 1)
 C_0[i_box] = C_i
 
+
 # matrix to store the transition of concentration
 Conc = matrix(nrow = n_box, ncol = length(t))
-
+Conc[, 1] = C_0
 # loop over time to calculate C(t) for t
 for (t_i in t) {
-  Lambda = exp_Lambda(e_vals, t_i)
-  conc_t_i = Re(Xe %*% Lambda %*% Xe.inv %*% C_0)
-  Conc[, t_i+1] = conc_t_i
-  png(paste("./part1_movie/", t_i, ".png", sep=""))
-  plot(x = 1:n_box, y = conc_t_i, xlim = c(1, n_box), ylim = c(0, C_i), xlab = "#box", ylab = "concentration")
-  dev.off()
+  if (t_i != 0) {
+    Lambda_t = exp_Lambda(e_vals, t_i)
+    conc_t_i = Re(Xe %*% Lambda_t %*% solve(Xe, C_0))
+    Conc[, t_i+1] = conc_t_i
+    filename = formatC(t_i, width = 3, format = "d", flag = "0")
+    png(paste("./part1_movie/", filename, ".png", sep=""))
+    plot(x = 1:n_box, y = conc_t_i, xlim = c(1, n_box), ylim = c(0, C_i), xlab = "#box", ylab = "concentration")
+    dev.off()
+  }
 }
-
 advection_diffusion_eqn = function(D, x, t_i) {
   exp_term = exp( -(x - v_a*t_i)^2 / (4*D*t_i) )
   return(1 / sqrt(4*pi*D*t_i) * exp_term)
@@ -90,12 +92,12 @@ advection_diffusion_eqn = function(D, x, t_i) {
 
 t_1 = 1:100
 box_num = 26
-plot(t, Conc[box_num,], ylim = c(0, C_i))
-d_list = seq(0.01, 0.05, 0.001)
+plot(t, Conc[box_num,], ylim = c(0, C_i), ylab = paste("conc of box", box_num))
+d_list = seq(0.01, 0.05, 0.0001)
 sqrt_err_list = rep(NA, length(d_list))
 idx = 1
 for (d_guess in d_list) {
-  diff = Conc[box_num,] - advection_diffusion_eqn(d_guess, abs(box_num - i_box), t_1)
+  diff = Conc[box_num, t_1] - advection_diffusion_eqn(d_guess, box_num - i_box, t_1)
   sqrt_err = sqrt(sum(diff^2))
   sqrt_err_list[idx] = sqrt_err
   idx = idx + 1
@@ -103,4 +105,4 @@ for (d_guess in d_list) {
 min_sqrt_err_idx = which(sqrt_err_list == min(sqrt_err_list))
 d_best = d_list[min_sqrt_err_idx]
 d_best
-lines(t_1, advection_diffusion_eqn(d_best, abs(box_num - i_box), t_1))
+lines(t_1, advection_diffusion_eqn(d_best, box_num - i_box, t_1))
